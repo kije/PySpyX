1f01b8b9b9a8b0ad62c4a22bcb7c2d7b498952ae
Kim Jeker <info@kije.ch>
2015-05-06 21:40:51 +0200

    UTF-8 / Copyright/GPL Header

--- PySpyX/urls.py
+++ PySpyX/urls.py
@@ -1,11 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+"""
+Camera Surveillance tool for Raspberry Pi with Camera module Project in ICT M152.
+Copyright (C) 2015  Kim D. Jeker
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__author__ = 'kije'
+
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-                       # Examples:
-                       # url(r'^$', 'PySpyX.views.home', name='home'),
-                       # url(r'^blog/', include('blog.urls')),
+urlpatterns = patterns(
+    '',
+    # Examples:
+    # url(r'^$', 'PySpyX.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
 
-                       url(r'^admin/', include(admin.site.urls)),
-                       url(r'^(index)?/?$', 'pyspy.views.index', name='index')
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^(?:index)?/?$', 'pyspy.views.index', name='index')
 )
--- pyspy/tests.py
+++ pyspy/tests.py
@@ -1,1 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+"""
+Camera Surveillance tool for Raspberry Pi with Camera module Project in ICT M152.
+Copyright (C) 2015  Kim D. Jeker
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__author__ = 'kije'
+
 # Create your tests here.
