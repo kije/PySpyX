456c92da8e30a61f97a68f543b148003414d7d38
Kim Jeker <info@kije.ch>
2015-05-06 22:47:31 +0200

    removed  comments

--- PySpyX/urls.py
+++ PySpyX/urls.py
@@ -26,9 +26,6 @@ from django.contrib import admin
 
 urlpatterns = patterns(
     '',
-    # Examples:
-    # url(r'^$', 'PySpyX.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^(?:index)?/?$', 'pyspy.views.index', name='index')
