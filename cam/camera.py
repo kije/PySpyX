e9574fec79dc5dd84dfbcb9f7cdf5200f70bd1bd
kije <github@kije.ch>
2015-06-15 21:39:54 +0200

    Script test improvements

--- cam/camera.py
+++ cam/camera.py
@@ -296,6 +296,7 @@ class LocalCamera(Camera):
         # check, if motion process is running
         for p in psutil.process_iter():
             try:
+                print(p.exe().lower())
                 p.exe().lower().index(self.CAM_MOTION_CMD)
                 return True
             except ValueError:
@@ -313,6 +314,7 @@ class LocalCamera(Camera):
         # check, if streaming process is running
         for p in psutil.process_iter():
             try:
+                print(p.exe().lower())
                 p.exe().lower().index(self.CAM_VLC_CMD)
 
                 return True
